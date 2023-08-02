0b29a8ac40c18f549200315da990cc0fd21d501f
Raul Diamantino <rauldiamantino25@gmail.com>
2023-08-01 22:48:14 -0300

    Feat: Exibe despesas na view expenses.php

--- app/views/expenses.php
+++ app/views/expenses.php
@@ -1,4 +1,22 @@
-<?php
-echo '<pre>';
-print_r($expenses);
-echo '</pre>';
+<section class="container mt-4">
+  <table class="table table-dark table-striped-columns">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Descrição</th>
+        <th>Valor</th>
+        <th>Data</th>
+      </tr>
+    </thead>
+    <tbody>
+    <?php foreach($expenses as $value): ?>
+      <tr>
+        <td><?php echo $value['id']; ?></td>
+        <td><?php echo $value['description']; ?></td>
+        <td><?php echo 'R$ ' . number_format($value['amount'], 2, ',', '.'); ?></td>
+        <td><?php echo $value['date']; ?></td>
+      </tr>
+    <?php endforeach; ?>
+    </tbody>
+  </table>
+</section>
