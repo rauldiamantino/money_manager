42eb2f75a92adaadabea1fb3db6e295837ee0e86
Raul Diamantino <rauldiamantino25@gmail.com>
2023-08-04 21:45:52 -0300

    Fix: Remove classe obsoleta

--- app/Database.php
+++ app/Database.php
@@ -2,55 +2,16 @@
 
 class Database
 {
-  protected $connection;
-  protected $host;
-  protected $database;
-  protected $username;
-  protected $password;
-
-  public function __construct($database_name = null)
-  {
-    $this->host = DB_HOST;
-    $this->database = $database_name ?? DB_NAME;
-    $this->username = DB_USER;
-    $this->password = DB_PASSWORD;
-
-    $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
-
-    if ($this->connection->connect_error) {
-      die('Falha na conexão com o banco de dados: ' . $this->connection->connect_error);
-    }
-  }
-
-  public function query($sql)
-  {
-    $result = $this->connection->query($sql);
-
-    if ($result === false) {
-      die('Erro na consulta: ' . $this->connection->error);
-    }
-
-    return $result;
-  }
-
-  public function close()
-  {
-    $this->connection->close();
-  }
-}
-
-class Database2
-{
   private $connection;
 
   public function __construct($host, $dbname, $username, $password)
   {
     try {
-      $this->connection = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+      $this->connection = new PDO('mysql:host=' . $host . ';dbname=' . $dbname . ';charset=utf8', $username, $password);
       $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } 
     catch (PDOException $e) {
-      die("Error connection: " . $e->getMessage());
+      die('Error connection: ' . $e->getMessage());
     }
   }
 
@@ -87,7 +48,7 @@ class Database2
       $stmt->execute();
     } 
     catch (PDOException $e) {
-      echo 'Error: ' . $e->getMessage();
+      die('Error: ' . $e->getMessage());
     }
 
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
--- app/dao/UsersDao.php
+++ app/dao/UsersDao.php
@@ -4,18 +4,16 @@ require_once '../app/Database.php';
 class UsersDAO
 {
   private $database;
-  private $database2;
 
   public function __construct()
   {
-    $this->database = new Database(); 
-    $this->database2 = new Database2(DB_HOST, DB_NAME, DB_USER, DB_PASSWORD);
+    $this->database = new Database(DB_HOST, DB_NAME, DB_USER, DB_PASSWORD);
   }
 
   public function get_user_db($email)
   {
     $params = ['where' => 'email = \'' . $email . '\''];
-    $result = $this->database2->select($params);
+    $result = $this->database->select($params);
     return $result;
   }
 
@@ -27,7 +25,7 @@ class UsersDAO
       'password' => password_hash($user_data['user_password'], PASSWORD_DEFAULT),
     ];
 
-    $result = $this->database2->insert('users', $user);
+    $result = $this->database->insert('users', $user);
     return $result;
   }
 }
