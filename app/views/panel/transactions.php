1f219b55fda3e3b517925dcb25c88906c694aa90
Raul Diamantino <rauldiamantino25@gmail.com>
2023-08-09 21:38:50 -0300

    Feat: Implementa Modal de Transação

--- app/views/panel/transactions.php
+++ app/views/panel/transactions.php
@@ -1,5 +1,10 @@
 <section class="container mt-4">
-  <h1>Geral</h1>
+  <h1 class="mb-4">Geral</h1>
+
+  <button type="button" class="btn btn-primary rounded-0 py-1 mb-2" data-toggle="modal" data-target="#modalExemplo">
+    Adicionar transação
+  </button>
+
   <table class="table table-dark table-striped-columns">
     <thead>
       <tr>
@@ -12,16 +17,37 @@
       </tr>
     </thead>
     <tbody>
-    <?php foreach($transactions as $value): ?>
-      <tr>
-        <td><?php echo $value['id']; ?></td>
-        <td><?php echo $value['description']; ?></td>
-        <td><?php echo $value['account_name']; ?></td>
-        <td><?php echo $value['category_name']; ?></td>
-        <td><?php echo 'R$ ' . number_format($value['amount'], 2, ',', '.'); ?></td>
-        <td><?php echo $value['date']; ?></td>
-      </tr>
-    <?php endforeach; ?>
+      <?php foreach ($transactions as $value) : ?>
+        <tr>
+          <td><?php echo $value['id']; ?></td>
+          <td><?php echo $value['description']; ?></td>
+          <td><?php echo $value['account_name']; ?></td>
+          <td><?php echo $value['category_name']; ?></td>
+          <td><?php echo 'R$ ' . number_format($value['amount'], 2, ',', '.'); ?></td>
+          <td><?php echo $value['date']; ?></td>
+        </tr>
+      <?php endforeach; ?>
     </tbody>
   </table>
 </section>
+
+<!-- Modal -->
+<div class="modal fade" id="modalExemplo" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Adicionar transação</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+        <button type="button" class="btn btn-primary">Salvar mudanças</button>
+      </div>
+    </div>
+  </div>
+</div>
